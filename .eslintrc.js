8aab52affef9e0274f40dd0e7993be969b092ad0
aashishpanchal <aipanchal51@gmail.com>
2024-11-17 06:25:19 +0530

    remove: no-require-imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,5 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-require-imports': 'off',
   },
 };
