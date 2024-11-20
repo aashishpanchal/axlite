ff6f39bdfb40f9c24804850c58f50f5c943261d6
aashishpanchal <aipanchal51@gmail.com>
2024-11-20 06:12:47 +0530

    remove: wrapper-controller

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,3 @@
-import {wrapper} from './factory';
 import {HttpError, InternalServerError} from './errors';
 import type {ErrorRequestHandler} from 'express';
 
@@ -6,8 +5,6 @@ type Options = {
   isDev?: boolean;
   write?: (error: unknown) => void;
 };
-type Constructor<T> = new (...args: any[]) => T;
-type ContainerType = 'local' | 'tsyringe' | 'typedi';
 
 /**
  * Express middleware to handle `HttpError` and unknown errors.
@@ -52,71 +49,4 @@ export const globalErrorHandler = (
     );
     return res.status(error.status).json(error.toJson());
   };
-};
-
-/**
- * Creates a controller object to manage route methods dynamically.
- *
- * @param cls - The controller class to instantiate.
- * @param type - The container type for resolving the instance.
- * @returns An object with `getMethod` for retrieving wrapped methods.
- */
-export const wrapperController = <T>(
-  cls: Constructor<T>,
-  type: ContainerType,
-) => {
-  const instance = resolve(cls, type);
-
-  /**
-   * Retrieves and wraps a method from the controller for routing.
-   *
-   * @param key - The method name to retrieve.
-   * @returns A wrapped request handler.
-   */
-  const getMethod = <K extends keyof T>(key: K) => {
-    // get handler from instance
-    const handler = instance[key];
-    if (typeof handler !== 'function')
-      throw new Error(
-        `The key '${String(key)}' is not a function in ${instance.constructor.name}.`,
-      );
-    // return handler with wrapper
-    return wrapper(handler.bind(instance));
-  };
-
-  return {getMethod};
-};
-
-/**
- * Resolves an instance of a class based on the container type.
- *
- * @param cls - The class constructor to resolve an instance of.
- * @param type - The container type (`'local'` or `'tsyringe'`).
- * @returns An instance of the given class resolved accordingly.
- */
-const resolve = <T>(cls: Constructor<T>, type: ContainerType): T => {
-  switch (type) {
-    case 'local':
-      return new cls();
-    case 'tsyringe':
-      try {
-        const {container} = require('tsyringe');
-        return container.resolve(cls);
-      } catch (error: any) {
-        throw new Error(
-          `Failed to resolve ${cls.name} with 'tsyringe'. Ensure 'tsyringe' is installed.\n${error.message}`,
-        );
-      }
-    case 'typedi':
-      try {
-        const {Container} = require('typedi');
-        return Container.get(cls);
-      } catch (error: any) {
-        throw new Error(
-          `Failed to resolve ${cls.name} with 'typedi'. Ensure 'typedi' is installed.\n${error.message}`,
-        );
-      }
-    default:
-      throw new Error(`Invalid container type: ${type}`);
-  }
 };
